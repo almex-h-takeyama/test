2293f1ad72004c7b04a287c31cf1ce67458dacfe
takeyama-h1 <h-takeyama@usen-next.jp>
2021-08-05 09:43:11 +0000

    stderr?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 set -eu
-RESULT=$(git-pr-release --squashed)
-# echo "$RESULT"
+git-pr-release --squashed 2>&1 | tee result.txt
 
-PR_URL=$(echo $RESULT | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
+PR_URL=$(grep -E "https://github.com/(.*)/pull/(.*)" result.txt | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
 echo "pr_url: $PR_URL"
 echo "::set-output name=pr_url::$PR_URL"
