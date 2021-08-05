31b29ab356b8ef2acdd50304d80ab97346fa8d5b
takeyama-h1 <h-takeyama@usen-next.jp>
2021-08-05 09:09:59 +0000

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 set -eu
-git-pr-release --squashed | tee result.txt
+RESULT=$(git-pr-release --squashed)
+echo "$RESULT"
 
-PR_URL=$(grep -E "https://github.com/(.*)/pull/(.*)" result.txt | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
+PR_URL=$(echo $RESULT | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
 echo "pr_url: $PR_URL"
 echo "::set-output name=pr_url::$PR_URL"
