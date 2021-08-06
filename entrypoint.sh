23a1ba9ec7ed215f40c710d334efb215e0fdf118
takeyama-h1 <h-takeyama@usen-next.jp>
2021-08-06 01:07:47 +0000

    cat動いてる？

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -eu
 git-pr-release --squashed 2>&1 > result.txt
-cat result.txt
+# cat result.txt
 
 PR_URL=$(grep -E "https://github.com/(.*)/pull/(.*)" result.txt | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
 echo "pr_url: $PR_URL"
