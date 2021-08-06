eb4e76251912f8ce7a230c9573ff59f6fb80a696
takeyama-h1 <h-takeyama@usen-next.jp>
2021-08-06 01:26:18 +0000

    update 色を消すsed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -eu
 git-pr-release --squashed > result.txt 2>&1
 cat result.txt
 
-PR_URL=$(grep -E "https://github.com/(.*)/pull/(.*)" result.txt | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
+PR_URL=$(cat result.txt | sed "s/^[\[[0-9;]*m//g" | grep -E "https://github.com/(.*)/pull/(.*)"  | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
 echo "pr_url: $PR_URL"
 echo "::set-output name=pr_url::$PR_URL"
