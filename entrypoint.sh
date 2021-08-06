9274aa1d54d00d8d2704d16b00c74c70e2adae1f
takeyama-h1 <h-takeyama@usen-next.jp>
2021-08-06 02:10:47 +0000

    正規表現ミスだった

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 set -eu
-git-pr-release --squashed > result.txt 2>&1
-cat -A result.txt
+git-pr-release --squashed 2>&1 | tee result.txt
 
-PR_URL=$(cat result.txt | sed "s/^[\[[0-9;]*m//g" | grep -E "https://github.com/(.*)/pull/(.*)"  | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
+PR_URL=$(grep -E "https://github.com/(.*)/pull/(.*)" result.txt | sed -r 's#^(.*)(https://github.com/.*/pull/[0-9])(.*)$#\2#')
 echo "pr_url: $PR_URL"
 echo "::set-output name=pr_url::$PR_URL"
-
-
-# cat result.txt | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | sed -r 's#(.*)(https://github.com/.*/pull/[0-9]*)#\2#'
