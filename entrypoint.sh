157be5c89281facfbe802adb0ac7ea5c1f0c5c61
takeyama-h1 <h-takeyama@usen-next.jp>
2021-08-06 00:57:54 +0000

    teeを使わなければどうか

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 set -eu
-git-pr-release --squashed 2>&1 | tee result.txt
+git-pr-release --squashed 2>&1 > result.txt
+cat result.txt
 
-PR_URL=$(cat result.txt | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
+PR_URL=$(grep -E "https://github.com/(.*)/pull/(.*)" result.txt | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
 echo "pr_url: $PR_URL"
 echo "::set-output name=pr_url::$PR_URL"
